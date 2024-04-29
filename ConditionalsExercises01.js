3a3ea5d9127be6497ef8ded1e70d04c65783ed33
Dhana bayya <krishnabayya@Dhanas-mac-Air.lan>
2024-04-29 11:39:16 -0500

    ConditionalExercies123files

---
+++ ConditionalsExercises01.js
@@ -0,0 +1,12 @@
+// Declare and initialize the variables for exercise 1 here:
+let engineIndicatorLight = "red blinking";
+let spaceSuitsOn = true;
+// BEFORE running the code, predict what will be printed to the console by the following statements:
+console.log("engines are off");
+if (engineIndicatorLight === "green") {
+  console.log("engines have started");
+} else if (engineIndicatorLight === "green blinking") {
+  console.log("engines are preparing to start");
+} else {
+  console.log("engines are off");
+}
